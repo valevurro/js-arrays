4a28d4f6e2bd7a4df1221684c18bc80a758f8ab9
valevurro <vale.v.00@outlook.it>
2025-07-14 19:42:56 +0200

    Add comments in the first assignment and complete second assignement

--- script.js
+++ script.js
@@ -10,11 +10,23 @@ const teachers = [
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
+
+//per poter stampare il valore del quarto insegnante vado ad utilizzarela variabile teachers e sapendo che si parte a contare da 0 vado ad utilizzare il numero 3
 const fourthTeacher = teachers[3];
+
+//il valore che viene stampato in console è Phil
 .console.log(fourthTeacher);
 
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+
+//vado ad utilizzare splice perchè va ad aggiungere o rimuovere elementi
+//visto che l'insegnante da sostituire è il quinto, andando in ordine, l'insegnante nella lista è il quarto
+//dobbiamo poi eliminare un elemento
+//bisogna sostituire 'Patrick'
+
+teachers.splice(4, 1, 'Patrick');
+console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = null;
